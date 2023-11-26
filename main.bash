a5da77c83898af65ad1ff62340472f6879e2c551
pjc <pjc@lp21>
2023-11-27 11:17:47 +1300

    init

---
+++ main.bash
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+clear
+echo 
+
+function install_homebrew() {
+  echo "===> install homebrew" 
+
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+}
+
+function brew_installs()
+  echo "===> brew_installs()"
+  local app_list=(
+    mc
+    micro
+  )
+
+  local app
+  for app in ${app_list}; do
+    echo 
+    echo "===> brew install ${app}"
+    brew install ${app}
+  done
+
+}
+
+function main() {
+  install_homebrew
+  brew_installs
+}
+
+main
+
+
