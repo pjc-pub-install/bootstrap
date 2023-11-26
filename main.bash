34c838b2b75b81e1ca08b0d0e41fd7a9d581e315
pjc <pjc@lp21>
2023-11-27 12:08:03 +1300

    init

--- main.bash
+++ main.bash
@@ -8,6 +8,13 @@ function install_homebrew() {
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+  echo "===> update .bashrc"
+  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/pjc/.bashrc
+
+  echo "===> update this shell"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+
 }
 
 function brew_installs() {
